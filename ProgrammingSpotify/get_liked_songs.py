c10460bedc5b10d644e2563b95f9c8ba316984b4
ShrijithR <72496419+ShrijithR@users.noreply.github.com>
2022-01-11 00:36:44 +0530

    Creating dataset, extracting songs and artists

--- ProgrammingSpotify/get_liked_songs.py
+++ ProgrammingSpotify/get_liked_songs.py
@@ -2,49 +2,55 @@ import pickle
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
-
+from creds import client_id, client_secret
 import pandas as pd
 
-client_id=""
-client_secret=""
-scope = "user-library-read"
-# spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
-#     client_id="",
-#     client_secret="",
-# ))
 
+scope = "user-library-read"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id="",
-    client_secret="", 
+    client_id=client_id,
+    client_secret=client_secret, 
     redirect_uri="http://127.0.0.1:9090", 
     username="Shrijith R",
     scope=scope))
-
-# def show_tracks(results):
-#     for item in results['items']:
-#         track = item['track']
-#         print("%32.32s %s" % (track['artists'][0]['name'], track['name']))
 
 limit = 20
 offset = 0
 
 def api_call():
-    return sp.current_user_saved_tracks(limit=2)
+    return sp.current_user_saved_tracks(limit=limit)
 
 results = api_call()
 
-def get_details(results, limit, offset, total):
-    item_list = list(results.items())
-    total_songs = results['total']
-    for i in range(0,2):
-        print(item_list[1][1][i]['track']['name'])
-        print(item_list[1][1][i]['track']['album']['artists'][0]['name'])
-        print(item_list[1][1][i]['track']['album']['name'])
 
-def update_dataset():
-    pd.read_csv("datasets\song_list.csv")
-    pass
+def get_details(results):
+    details = []
+    item_list = list(results.items())
+    # total_songs = results['total']
+    for i in range(0,limit):
+        artist_name = item_list[1][1][i]['track']['name']
+        song_name= item_list[1][1][i]['track']['album']['artists'][0]['name']
+        details.append({'artist': artist_name, 'song': song_name})
+        # artist_dict[artist_name]['album'] = item_list[1][1][i]['track']['album']['name']
+    return details
+details_list = get_details(results)
+# print(details)
+
+def create_dataset(details_list):
+    # df = pd.read_csv('datasets/details.csv')
+    # df = pd.DataFrame(columns=['song', 'artist'])
+    # list_for_df = [[i['song'], i['artist']] for i in details_list]
+    # print(list_for_df)
+    # df.append(list_for_df)
+    # print(df)
+    df = pd.DataFrame(details_list)
+    df.to_csv("details.csv")
+    # print("done")
+create_dataset(details_list)
+# def update_dataset():
+#     pd.read_csv("datasets\song_list.csv")
+#     pass
 
 # with open("saved_tracks_2.py", "w") as file:
 #     file.write(str(results))

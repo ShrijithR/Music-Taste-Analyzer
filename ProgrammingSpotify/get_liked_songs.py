20fce352064e98caf8f48b33fc211873d3f1a080
ShrijithR <72496419+ShrijithR@users.noreply.github.com>
2022-01-12 22:06:07 +0530

    Added methods to get all teh songs from SPotify
    Added logic to get all entire song list from Spotify and extract all the songs.

--- ProgrammingSpotify/get_liked_songs.py
+++ ProgrammingSpotify/get_liked_songs.py
@@ -1,10 +1,10 @@
 import pickle
+from typing import ItemsView
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 from creds import client_id, client_secret
 import pandas as pd
-
 
 scope = "user-library-read"
 
@@ -15,42 +15,38 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     username="Shrijith R",
     scope=scope))
 
+def get_data_spotify(limit, offset):
+    item_list = []
+    while True:
+        results = sp.current_user_saved_tracks(limit=limit, offset=offset)
+        results_list = list(results.items())
+        item_list += results_list[1][1]
+        total_songs = results['total']
+        # total_songs = 6
+        offset += limit
+        # songs_collected = offset+limit
+        if offset>total_songs:
+            break
+        
+    return item_list, total_songs
+
+def extract_and_add(item_list, songs_collected):
+    details = []
+    for i in range(0,songs_collected):
+        artist_name = item_list[i]['track']['name']
+        song_name= item_list[i]['track']['album']['artists'][0]['name']
+        details.append({'artist': artist_name, 'song': song_name})
+        # artist_dict[artist_name]['album'] = item_list[1][1][i]['track']['album']['name']
+    
+    return details
+
+def create_dataset(details_list):
+    df = pd.DataFrame(details_list)
+    df.to_csv("details.csv")
+
 limit = 20
 offset = 0
 
-def api_call():
-    return sp.current_user_saved_tracks(limit=limit)
-
-results = api_call()
-
-
-def get_details(results):
-    details = []
-    item_list = list(results.items())
-    # total_songs = results['total']
-    for i in range(0,limit):
-        artist_name = item_list[1][1][i]['track']['name']
-        song_name= item_list[1][1][i]['track']['album']['artists'][0]['name']
-        details.append({'artist': artist_name, 'song': song_name})
-        # artist_dict[artist_name]['album'] = item_list[1][1][i]['track']['album']['name']
-    return details
-details_list = get_details(results)
-# print(details)
-
-def create_dataset(details_list):
-    # df = pd.read_csv('datasets/details.csv')
-    # df = pd.DataFrame(columns=['song', 'artist'])
-    # list_for_df = [[i['song'], i['artist']] for i in details_list]
-    # print(list_for_df)
-    # df.append(list_for_df)
-    # print(df)
-    df = pd.DataFrame(details_list)
-    df.to_csv("details.csv")
-    # print("done")
+item_list, songs_collected = get_data_spotify(limit, offset)
+details_list = extract_and_add(item_list, songs_collected)
 create_dataset(details_list)
-# def update_dataset():
-#     pd.read_csv("datasets\song_list.csv")
-#     pass
-
-# with open("saved_tracks_2.py", "w") as file:
-#     file.write(str(results))
